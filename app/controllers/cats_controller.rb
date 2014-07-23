25bbe7f9a89fc81619b709a50c4deef06997fc8f
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-23 11:55:04 -0400

    Add remaining actions to cats controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,6 @@
 class CatsController < ApplicationController
   respond_to :json
-  before_action :find_cat, only: :show
+  before_action :find_cat, only: [:show, :update, :destroy]
 
   def show
     respond_with @cat
@@ -11,8 +11,28 @@ class CatsController < ApplicationController
     respond_with @cats
   end
 
+  def create
+    @cat = Cat.new(cat_params)
+    @cat.save
+    respond_with @cat
+  end
+
+  def update
+    @cat.update(cat_params)
+    respond_with @cat
+  end
+
+  def destroy
+    @cat.destroy
+    respond_with @cat
+  end
+
   private
   def find_cat
     @cat = Cat.find(params[:id])
+  end
+
+  def cat_params
+    params.require(:cat).permit!
   end
 end
